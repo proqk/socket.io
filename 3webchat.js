51d151b0476c98f3e320c32dd3cbbe12881a8d28
Yujin Lee <proqk@naver.com>
2016-09-12 22:26:03 +0900

    3일차 오류수정
    다른 웹페이지에서 글이 안보이던 오류 수정

---
+++ 3webchat.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+
+var server = http.createServer(function (request, response){
+	fs.readFile('index.html', function (error, data){
+		response.writeHead(200, { 'Content-Type': 'text/html' });
+		response.end(data);
+	});
+}).listen(52273, function(){
+	console.log('Run!');
+});
+
+var io = socketio.listen(server);
+//io.set('log level', 2);
+
+io.sockets.on('connection', function (socket){
+	socket.on('message', function (data){
+//		console.log('clinet send data!', data);
+		io.sockets.emit('message', data);
+	});
+});
